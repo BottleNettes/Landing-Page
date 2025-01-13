fdcfffc1704c87987f6878d290fd5abc95ed0345
Mark <mark.kietnghiem@gmail.com>
2025-01-13 15:04:04 -0800

    refactor: Created SourcesList.jsx, removed subtitle.jsx

---
+++ src/components/SourcesList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SourcesList = () => {
+	return;
+};
+
+export default SourcesList;
--- src/components/subtitle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Subtitle = () => {
-	return;
-};
-
-export default Subtitle;
