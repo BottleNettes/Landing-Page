25f0efafebfbfefd4dbb88396ea7be3a0bd2d324
Julie Hoagland-Sorensen <143046484+JulieHoaglandSorensen@users.noreply.github.com>
2025-01-15 05:18:12 -0500

    This has been a long night

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ErrorBoundary from './components/ErrorBoundary';
 import App from './App';
-import "./styles/index.css"
+import './styles/index.css';
+import background from './assets/background.mp4';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ErrorBoundary>
+			<video
+				src={background}
+				autoPlay
+				loop
+				muted
+				style={{
+					position: 'fixed',
+					top: 0,
+					left: 0,
+					width: '100vw',
+					height: '100vh',
+					objectFit: 'cover',
+					zIndex: -1, // Keeps it behind all other content
+				}}
+			/>
+			<App />
+		</ErrorBoundary>
+	</React.StrictMode>
 );
