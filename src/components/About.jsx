6d373daa656462746389dd0f26a41c9df91c0bd7
Mark <mark.kietnghiem@gmail.com>
2025-01-16 00:07:56 -0800

    refactor: Cleaned up Dependencies.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
 
 const About = () => (
-  <section id='about' className='py-20 bg-transparent'>
-    <div className='mx-auto max-w-screen-xl px-4'>
-      <div className='mb-16'>
-        <h2 className='text-4xl font-bold text-[#072B51] text-center'>
-          About BottleNetes
-        </h2>
-        <br />
-      </div>
+	<section
+		id='about'
+		className='py-20 bg-transparent'
+	>
+		<div className='mx-auto max-w-screen-xl px-4'>
+			<div className='mb-16'>
+				<h2 className='text-4xl font-bold text-[#072B51] text-center'>
+					About BottleNetes
+				</h2>
+				<br />
+			</div>
 
-      <div className='about-video-container backdrop-blur-sm bg-white/30 rounded-xl overflow-hidden shadow-[0_8px_30px_rgb(0,0,0,0.12)] mb-16'>
-        <iframe
-          // src='https://www.youtube.com/embed/KN3IemyqJJw'
-          src='https://www.youtube.com/embed/8QLYp1AubWg'
-          title='BottleNetes Introduction'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-          loading='lazy'
-        ></iframe>
-      </div>
-      <br />
-      <div className='text-center'>
-        <a
-          href='https://github.com/oslabs-beta/BottleNetes'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='get-started-btn'
-        >
-          Get Started
-        </a>
-      </div>
-    </div>
-  </section>
+			<div className='about-video-container backdrop-blur-sm bg-white/30 rounded-xl overflow-hidden shadow-[0_8px_30px_rgb(0,0,0,0.12)] mb-16'>
+				<iframe
+					src='https://www.youtube.com/embed/0VBCYREW67s'
+					title='BottleNetes Introduction'
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+					allowFullScreen
+					loading='lazy'
+				></iframe>
+			</div>
+			<br />
+			<div className='text-center'>
+				<a
+					href='https://github.com/oslabs-beta/BottleNetes'
+					target='_blank'
+					rel='noopener noreferrer'
+					className='get-started-btn'
+				>
+					Get Started
+				</a>
+			</div>
+		</div>
+	</section>
 );
 
 export default About;
